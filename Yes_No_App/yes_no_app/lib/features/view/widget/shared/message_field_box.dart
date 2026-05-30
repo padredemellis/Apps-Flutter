c95ca737a7336b341b713a61ae1e8feda1f7a165
padredemellis <luisemanuelromeroduarte@gmail.com>
2026-05-30 02:22:39 -0300

    Yes no app vista

---
+++ Yes_No_App/yes_no_app/lib/features/view/widget/shared/message_field_box.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('button: $textValue');
+          textController.clear();
+        },
+        icon: Icon(Icons.send_and_archive_outlined),
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) => focusNode.unfocus(),
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("Submit value $value");
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
